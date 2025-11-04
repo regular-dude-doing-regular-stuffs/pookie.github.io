9afa5705cde4adee2f29022ce4e60add4fcc3860
regular-dude-doing-regular-stuffs <firstclasssingularity9@gmail.com>
2025-11-04 19:45:39 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+{
+  "name": "Pookie Boss Timer",
+  "short_name": "PookieTimer",
+  "start_url": "index.html",
+  "display": "standalone",
+  "background_color": "#013220",
+  "theme_color": "#013220",
+  "icons": [
+    {
+      "src": "pookie-icon.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
