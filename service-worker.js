73a077766b9dd227d156844cf79a56fb32e6bf97
regular-dude-doing-regular-stuffs <firstclasssingularity9@gmail.com>
2025-11-06 07:02:17 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,56 @@
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open('pookie-v1').then(cache => cache.addAll(['/', '/index.html', '/manifest.json'])));
+const CACHE_NAME = "boss-timer-cache-v1";
+const ASSETS = [
+"./",
+"./index.html",
+"./manifest.json",
+"./icons/icon-192.png",
+"./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+event.waitUntil(
+caches.open(CACHE_NAME).then((cache) => {
+console.log("📦 Caching app shell");
+return cache.addAll(ASSETS);
+})
+);
+self.skipWaiting();
 });
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
+
+self.addEventListener("activate", (event) => {
+event.waitUntil(
+caches.keys().then((keys) =>
+Promise.all(keys.map((key) => key !== CACHE_NAME && caches.delete(key)))
+)
+);
+self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+const url = new URL(event.request.url);
+
+// Always try network first for the JSON data
+if (url.href.includes("jsonbin.io")) {
+event.respondWith(
+fetch(event.request).catch(() => caches.match(event.request))
+);
+return;
+}
+
+// Cache-first for everything else
+event.respondWith(
+caches.match(event.request).then((cached) => {
+return (
+cached ||
+fetch(event.request)
+.then((res) => {
+return caches.open(CACHE_NAME).then((cache) => {
+cache.put(event.request, res.clone());
+return res;
+});
+})
+.catch(() => caches.match("./index.html"))
+);
+})
+);
 });
