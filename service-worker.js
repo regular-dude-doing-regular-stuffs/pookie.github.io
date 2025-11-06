60f7f40aafb2ea12830fd1cc1bf8239ed9a62295
regular-dude-doing-regular-stuffs <firstclasssingularity9@gmail.com>
2025-11-06 09:08:51 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,56 +1,39 @@
-const CACHE_NAME = "boss-timer-cache-v1";
-const ASSETS = [
-"./",
-"./index.html",
-"./manifest.json",
-"./icons/icon-192.png",
-"./icons/icon-512.png"
+const CACHE_NAME = "pookie-boss-timer-v3";
+const URLS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
-self.addEventListener("install", (event) => {
-event.waitUntil(
-caches.open(CACHE_NAME).then((cache) => {
-console.log("📦 Caching app shell");
-return cache.addAll(ASSETS);
-})
-);
-self.skipWaiting();
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
 });
 
-self.addEventListener("activate", (event) => {
-event.waitUntil(
-caches.keys().then((keys) =>
-Promise.all(keys.map((key) => key !== CACHE_NAME && caches.delete(key)))
-)
-);
-self.clients.claim();
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return (
+        response ||
+        fetch(event.request).catch(() => caches.match("./index.html"))
+      );
+    })
+  );
 });
 
-self.addEventListener("fetch", (event) => {
-const url = new URL(event.request.url);
-
-// Always try network first for the JSON data
-if (url.href.includes("jsonbin.io")) {
-event.respondWith(
-fetch(event.request).catch(() => caches.match(event.request))
-);
-return;
-}
-
-// Cache-first for everything else
-event.respondWith(
-caches.match(event.request).then((cached) => {
-return (
-cached ||
-fetch(event.request)
-.then((res) => {
-return caches.open(CACHE_NAME).then((cache) => {
-cache.put(event.request, res.clone());
-return res;
-});
-})
-.catch(() => caches.match("./index.html"))
-);
-})
-);
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
+  );
 });
